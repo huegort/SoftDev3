95e377a4ed09adda00e5affc6a11161b0093816a
ITS <ITS@10.0.2.15>
2016-10-20 02:24:46 +0100

    asdg

--- SoftDev3_samplecode/src/ie/gmit/dao/BookDAO.java
+++ SoftDev3_samplecode/src/ie/gmit/dao/BookDAO.java
@@ -3,7 +3,7 @@ package ie.gmit.dao;
 import java.util.List;
 
 import ie.gmit.domain.Book;
-// the basic methods
+
 public interface BookDAO {
 	public void insert(Book book);
 	public void update(Book book);
